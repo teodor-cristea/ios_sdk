9b26ca70c35a5fb0a72329c0fae0008ef0fc0742
tageborg <tage@Tages-MacBook-Air-2.local>
2013-09-16 10:44:08 +0200

    Fix for malformed EAS_UIDs and null param values and more
    * Moved NSLog statement of URL to after all elements have been appended
    * Increased precision of lat/long geolocation (not sure if device
    provides more than 32bit double, but just to make sure it's not
    truncated)
    * Tested with different locales (en_US vs sv_SE) to ensure that
    lat/long is always reported with decimal period, not comma
    * Code now checks for null-valued parameters to URL and do not append
    them - no more http://.../eas?xxx=(null) in logs or parsing
    * Removed UDID as method for generating eas_uid
    * Added eas_uid generation as per Classroom instructions --
    concat(unix_ts, rand(100000000, 999999999))
    	- uses arc4random_uniform(upper_limit) which may break IOS 4.x
    compatibility, but it arc4random was recommended all over the place...
    * Changed key for storage of eas_uid in NSUserDefaults from UDID to
    EAS_UID
    * We remove any previously stored UDID from NSUserDefaults
    * Killed som whitespace

--- sdk/Public/EmediateAdView.m
+++ sdk/Public/EmediateAdView.m
@@ -21,94 +21,94 @@
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) 
+    if (self)
     {
-      // Initialization code
-      parameters = nil;
-      refreshRate = -1;
+        // Initialization code
+        parameters = nil;
+        refreshRate = -1;
     }
     return self;
 }
 
 - (void)dismissAlerts
 {
-  UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-  NSArray *subviews = [window subviews];
-  if (subviews > 0)
-  {
-    UIAlertView *alert = [subviews objectAtIndex:0];
-    if ([alert isKindOfClass:[UIAlertView class]])
+    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+    NSArray *subviews = [window subviews];
+    if (subviews > 0)
     {
-      [alert dismissWithClickedButtonIndex:[alert cancelButtonIndex] animated:NO];
+        UIAlertView *alert = [subviews objectAtIndex:0];
+        if ([alert isKindOfClass:[UIAlertView class]])
+        {
+            [alert dismissWithClickedButtonIndex:[alert cancelButtonIndex] animated:NO];
+        }
     }
-  }
 }
 
 - (void)stopRefresh
 {
-  if (refreshTimer && [refreshTimer isValid])
-  {
-    [refreshTimer invalidate];
-    [refreshTimer release];
-    refreshTimer = nil;
-  }
+    if (refreshTimer && [refreshTimer isValid])
+    {
+        [refreshTimer invalidate];
+        [refreshTimer release];
+        refreshTimer = nil;
+    }
 }
 
 - (void)dealloc
 {
-  [self dismissAlerts];
-  
-  if (refreshTimer && [refreshTimer isValid])
-  {
-    [refreshTimer invalidate];
-    [refreshTimer release];
-    refreshTimer = nil;
-  }
-  
-  if (parameters)
-  {
-    [parameters release];
-    parameters = nil;
-  }
-  
-  [super dealloc];
+    [self dismissAlerts];
+    
+    if (refreshTimer && [refreshTimer isValid])
+    {
+        [refreshTimer invalidate];
+        [refreshTimer release];
+        refreshTimer = nil;
+    }
+    
+    if (parameters)
+    {
+        [parameters release];
+        parameters = nil;
+    }
+    
+    [super dealloc];
 }
 
 - (void)fetchCampaignWithRefreshRate:(NSInteger)refresh
 {
-  self.refreshRate = refresh;  
+    self.refreshRate = refresh;
 }
 
 - (void)stop
 {
-  self.parameters = nil;
-  
-  if (refreshTimer && [refreshTimer isValid])
-  {
-    [refreshTimer invalidate];
-    [refreshTimer release];
-    refreshTimer = nil;
-  }
+    self.parameters = nil;
+    
+    if (refreshTimer && [refreshTimer isValid])
+    {
+        [refreshTimer invalidate];
+        [refreshTimer release];
+        refreshTimer = nil;
+    }
 }
 
 - (void)refreshCreative
 {
-  [self loadCreativeWithParameters:nil];
+    [self loadCreativeWithParameters:nil];
 }
 
 - (void)loadCreativeWithParameters:(NSDictionary *)params
 {
     
-//    [self loadCreativeInternalWithParameters:params];
+    //    [self loadCreativeInternalWithParameters:params];
     
     if([CLLocationManager locationServicesEnabled] &&
        [CLLocationManager authorizationStatus] != kCLAuthorizationStatusDenied){
         if(self.userLocation){ // done waiting
-//            NSLog(@"done waiting, params = %@",params);
+            //            NSLog(@"done waiting, params = %@",params);
             [self loadCreativeInternalWithParameters:params];
             
         }else{// wait for user location
-//            NSLog(@"waiting");
+            //            NSLog(@"waiting");
             [self performSelector:@selector(loadCreativeWithParameters
                                             :) withObject:params afterDelay:0.2f];
             
@@ -118,7 +118,7 @@
             }
         }
     }else{
-//        NSLog(@"not wait");
+        //        NSLog(@"not wait");
         [self loadCreativeInternalWithParameters:params];
         
         
@@ -128,108 +128,122 @@
 
 - (void)loadCreativeInternalWithParameters:(NSDictionary *)params
 {
-        
-    
-  if ((params != nil) && (parameters == nil)) //When loading the ad for the 1st time.
-  {
-    self.parameters = params;
-  }
-    NSLog(@"self.parameters = %@",self.parameters);
-  //Format the URL and initiate load the creative  
-  NSMutableString *completeURLString = nil;
-  if (self.parameters != nil)
-  {
-    NSArray *keys = [self.parameters allKeys];
-  
-    if ([keys count] != 0)
+    if ((params != nil) && (parameters == nil)) //When loading the ad for the 1st time.
     {
-      completeURLString = [[NSMutableString alloc] initWithString:[baseURL hasSuffix:@"?"] ? baseURL : [baseURL stringByAppendingString:@"?"]];
-      for (NSString *key in keys)
-      {
-        [completeURLString appendFormat:@"%@=%@;", key, [self.parameters valueForKey:key]];
-      }
+        self.parameters = params;
+    }
+    NSLog(@"self.parameters = %@",self.parameters);
+    //Format the URL and initiate load the creative
+    NSMutableString *completeURLString = nil;
+    if (self.parameters != nil)
+    {
+        NSArray *keys = [self.parameters allKeys];
         
-        if(self.userLocation){
+        if ([keys count] != 0)
+        {
+            completeURLString = [[NSMutableString alloc] initWithString:[baseURL hasSuffix:@"?"] ? baseURL : [baseURL stringByAppendingString:@"?"]];
+            for (NSString *key in keys)
+            {
+                if ([self.parameters valueForKey:key]) // CTO inefficiency...
+                {
+                    [completeURLString appendFormat:@"%@=%@;", key, [self.parameters valueForKey:key]];
+                }
+            }
             
-            [completeURLString appendFormat:@"lat=%f;", self.userLocation.coordinate.latitude];
-            [completeURLString appendFormat:@"lng=%f;", self.userLocation.coordinate.longitude];
+            if(self.userLocation){
+                // use %lf instead of %f to ensure enough precision
+                [completeURLString appendFormat:@"lat=%lf;", self.userLocation.coordinate.latitude];
+                [completeURLString appendFormat:@"lng=%lf;", self.userLocation.coordinate.longitude];
+            }
         }
     }
-    NSLog(@"completeURLString = %@", completeURLString);
-  }
-  
-  if (completeURLString && [completeURLString length])
-  {
-    NSString *udid = [[NSUserDefaults standardUserDefaults] objectForKey:@"UDID"];
-    if (!udid) //If UDID is not stored in UserDefaults before, save in UserDefaults. 
-    {
-      udid = [TMLOpenUDID value];
-      [[NSUserDefaults standardUserDefaults] setObject:udid forKey:@"UDID"]; 
-      [[NSUserDefaults standardUserDefaults] synchronize];
-    }
-      
-    [completeURLString appendFormat:@"%@=%@", @"eas_uid", udid];
     
-    NSURL *url = [[NSURL alloc] initWithString:completeURLString];
-    [self loadCreative:url];
-    [url release];
-    url = nil;
-  }
-
-  if (completeURLString)
-  {
-    [completeURLString release];
-    completeURLString = nil;
-  }
+    if (completeURLString && [completeURLString length])
+    {
+        // Previously used UDID is no good for our purposes, instead use uid generation algoritm as described on
+        // http://classroom.emediate.com/doku.php?id=technical:in_depth_descriptions:cookie-less_requests
+        
+        NSString *eas_uid = [[NSUserDefaults standardUserDefaults] objectForKey:@"EAS_UID"];
+        
+        if (!eas_uid) //If EAS_UID is not stored in UserDefaults before, save in UserDefaults and delete UDID if stored there
+        {
+            if ([[NSUserDefaults standardUserDefaults] objectForKey:@"UDID"])
+            {
+                [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"UDID"];
+            }
+            
+            time_t unix_time = time(nil);
+            srandom(unix_time);
+            long r = (long)(100000000 + arc4random_uniform(900000000)); // precisely nine digits
+            
+            NSMutableString *eas_uid = [[[NSMutableString alloc] init] autorelease];
+            [eas_uid appendFormat:@"%lu", unix_time];
+            [eas_uid appendFormat:@"%lu", r];
+            
+            [[NSUserDefaults standardUserDefaults] setObject:eas_uid forKey:@"EAS_UID"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+        
+        [completeURLString appendFormat:@"%@=%@", @"eas_uid", eas_uid];
+        
+        NSURL *url = [[NSURL alloc] initWithString:completeURLString];
+        [self loadCreative:url];
+        [url release];
+        url = nil;
+    }
+    
+    if (completeURLString)
+    {
+        [completeURLString release];
+        completeURLString = nil;
+    }
 }
 
 - (void)fireAdWillShow
 {
-  if (!refreshTimer)
-  {
-      refreshTimer = [NSTimer scheduledTimerWithTimeInterval:self.refreshRate target:self selector:@selector(refreshCreative) userInfo:nil repeats:YES];
-      [refreshTimer retain];
-  }
+    if (!refreshTimer)
+    {
+        refreshTimer = [NSTimer scheduledTimerWithTimeInterval:self.refreshRate target:self selector:@selector(refreshCreative) userInfo:nil repeats:YES];
+        [refreshTimer retain];
+    }
     
-    
-    
-  if([super respondsToSelector:@selector(fireAdWillShowCalledFromChildView)]){
-    [super performSelector:@selector(fireAdWillShowCalledFromChildView)];
-  }
+    if([super respondsToSelector:@selector(fireAdWillShowCalledFromChildView)]){
+        [super performSelector:@selector(fireAdWillShowCalledFromChildView)];
+    }
 }
 
 - (void)fireAppShouldSuspend
 {
-  if (refreshTimer)
-  {
-    [refreshTimer invalidate];
-    [refreshTimer release];
-    refreshTimer = nil;
-  }
-  if([super respondsToSelector:@selector(fireAppShouldSuspendCalledFromChildView)]){
-    [super performSelector:@selector(fireAppShouldSuspendCalledFromChildView)];
-  }
+    if (refreshTimer)
+    {
+        [refreshTimer invalidate];
+        [refreshTimer release];
+        refreshTimer = nil;
+    }
+    if([super respondsToSelector:@selector(fireAppShouldSuspendCalledFromChildView)]){
+        [super performSelector:@selector(fireAppShouldSuspendCalledFromChildView)];
+    }
 }
 
 
 - (void)fireAppShouldResume
 {
-  if (refreshTimer)
-  {
-    [refreshTimer invalidate];
-    [refreshTimer release];
-    refreshTimer = nil;
-  }
-  
-  if (self.refreshRate > 0)
-  {
-    refreshTimer = [NSTimer scheduledTimerWithTimeInterval:self.refreshRate target:self selector:@selector(refreshCreative) userInfo:nil repeats:YES];          
-    [refreshTimer retain];
-  }
+    if (refreshTimer)
+    {
+        [refreshTimer invalidate];
+        [refreshTimer release];
+        refreshTimer = nil;
+    }
     
-  if([super respondsToSelector:@selector(fireAppShouldResumeCalledFromChildView)]){
-    [super performSelector:@selector(fireAppShouldResumeCalledFromChildView)];
-  }
+    if (self.refreshRate > 0)
+    {
+        refreshTimer = [NSTimer scheduledTimerWithTimeInterval:self.refreshRate target:self selector:@selector(refreshCreative) userInfo:nil repeats:YES];
+        [refreshTimer retain];
+    }
+    
+    if([super respondsToSelector:@selector(fireAppShouldResumeCalledFromChildView)]){
+        [super performSelector:@selector(fireAppShouldResumeCalledFromChildView)];
+    }
 }
 
 
